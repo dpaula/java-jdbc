87ffb8623fba65389450543bc3a0b034178c8468
dpaula <fernando.dpaula@gmail.com>
2018-07-22 18:42:48 -0300

    projeto base

---
+++ src/com/dpaula/jdbc/TestaConexao.java
@@ -0,0 +1,17 @@
+package com.dpaula.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class TestaConexao {
+
+	public static void main(String[] args) throws SQLException {
+
+		Connection connection = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/loja-virtual", "SA", "");
+		System.out.println("Abrindo uma conexão");
+
+		connection.close();
+	}
+
+}
